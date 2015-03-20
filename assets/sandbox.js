d34a8cf714ce9588ae37e7b9dae333f49b3d76c6
Michael Strickland <michael.c.strickland@gmail.com>
2015-03-19 19:01:45 -0700

    Added sandbox

---
+++ assets/sandbox.js
@@ -0,0 +1,26 @@
++function() {
+  'use strict';
+  var amls = Array.prototype.slice.call(document.getElementsByTagName('aml'));
+  amls.forEach(function(aml) {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'example');
+    div.appendChild(document.createElement('div'));
+    div.appendChild(document.createElement('div'));
+    div.children[0].appendChild(document.createElement('textarea'));
+    div.children[1].appendChild(document.createElement('pre'));
+    var content = aml.innerText || aml.childNodes[0].data;
+    div.children[0].children[0].value = content.replace(/(^\n)|(\n$)/g, '');
+
+    var parent = aml.parentNode;
+    parent.insertBefore(div, aml.nextSibling);
+    parent.removeChild(aml);
+
+    var source = div.children[0].children[0];
+    var destination = source.parentNode.parentNode.children[1].children[0];
+    source.style['min-height'] = (source.scrollHeight + 2) + 'px';
+    source.addEventListener('keyup', function(e) {
+      destination.innerHTML = JSON.stringify(archieml.load(source.value), null, 2);
+    });
+    source.dispatchEvent(new Event('keyup'));
+  });
+}();
